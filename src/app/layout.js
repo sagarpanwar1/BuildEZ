510d554270eae839833903f4f5fdb37579833ece
Pratik <pratikkhatri520@gmail.com>
2024-11-20 13:51:21 +0530

    fix build error

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
